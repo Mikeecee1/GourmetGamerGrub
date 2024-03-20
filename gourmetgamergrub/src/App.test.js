ff6769216a57de917e96b70c2388fdf3e2e65d41
Mikeecee1 <94531783+Mikeecee1@users.noreply.github.com>
2024-03-20 18:34:03 +0000

    test set up
    added skeleton tests to app test - planning how to implement

--- gourmetgamergrub/src/App.test.js
+++ gourmetgamergrub/src/App.test.js
@@ -1,8 +1,35 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+test("adds item to basket", () => {
+
+})
+
+test("removes item from basket", () => {
+
+})
+
+test("increments in basket", () =>{
+
+})
+
+test("decrements item in basket", () =>{
+
+})
+
+test("Updates total correctly when item added", () =>{
+
+})
+
+test("Updates total correctly when item removed", () => {
+  
+})
+

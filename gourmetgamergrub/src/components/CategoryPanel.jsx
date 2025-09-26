d0254b88cb2554286719c21c4911876245079b25
Mikeecee1 <mikeecee@hotmail.com>
2025-09-26 20:23:21 +0100

    ALtered display settings to optimise for different screens

--- gourmetgamergrub/src/components/CategoryPanel.jsx
+++ gourmetgamergrub/src/components/CategoryPanel.jsx
@@ -4,20 +4,14 @@ import './CategoryPanelStyles.css'
 function CategoryPanel(props){
     //functions here
 
-    
-
     //html
     return(
         <div className='categories' onClick={props.selectedMenu}>
-           
-            
-                <img id='image1'  src={props.catImage[0]}/>
-                <img id='image2'  src={props.catImage[1]}/>
+            <div className='categories-content'>
+                <img id='image1' src={props.catImage[0]} alt={`${props.menuName} food 1`}/>
                 <h2>{props.menuName}</h2>
-                
-
-                
-        
+                <img id='image2' src={props.catImage[1]} alt={`${props.menuName} food 2`}/>
+            </div>
         </div>
     )
 }
--- gourmetgamergrub/src/components/FoodItemPanel.jsx
+++ gourmetgamergrub/src/components/FoodItemPanel.jsx
@@ -1,29 +1,26 @@
 // will contain menu/ categories and items in each category when toggled
 import './FoodItemPanelStyles.css'
+
 function FoodItemPanel(props){
-//vars & functions
+    //vars & functions
 
-
-//html
-return(
-    <div className='FoodItemComp'>
-        
-        <img src={props.foodImage} ></img>
-        
-        <div className='FoodItemText'>
-            <h1>{props.foodName}{'    '} <b>£{props.foodPrice.toFixed(2)}</b></h1>  
-            <p id='desc'>{props.foodDesc} </p>
-           
-        </div>
-        
-        <div className='FoodItemButtons'>
-            <button id='AddItem' onClick={props.addItem}>+</button>
-            <button id='RemoveItem' onClick={props.removeItem}>-</button>
+    //html
+    return(
+        <div className='FoodItemComp'>
+            <img src={props.foodImage} alt={props.foodName}></img>
             
+            <div className='FoodItemText'>
+                <h1>{props.foodName}</h1>
+                <p id='desc'>{props.foodDesc}</p>
+                <p>£{props.foodPrice.toFixed(2)}</p>
+            </div>
+            
+            <div className='FoodItemButtons'>
+                <button id='AddItem' onClick={props.addItem} aria-label={`Add ${props.foodName} to basket`}>+</button>
+                <button id='RemoveItem' onClick={props.removeItem} aria-label={`Remove ${props.foodName} from basket`}>-</button>
+            </div>
         </div>
-    </div>
-)
-
+    )
 }
 
 export default FoodItemPanel
